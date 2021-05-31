744d842e24a0fffeedc86ad5096f12a268dbdcc2
Saimun <saimun4u@gmail.com>
2021-05-31 00:45:06 -0500

    Select button elements

---
+++ app.js
@@ -0,0 +1,9 @@
+//Select DOM Elements
+
+const main = document.getElementById('main');
+const addUser = document.getElementById('add-user');
+const doubleMoney = document.getElementById('double');
+const showMillionaires = document.getElementById('show-millionaires');
+const sortMillionaires = document.getElementById('sort');
+const calculateWealth = document.getElementById('calculate-wealth');
+
